a77787c4a1ab1de64cfe8566a758003393ab983d
Matt Ravenmoore <M_ravenmoore@outlook.com>
2020-08-22 16:15:18 -0700

    5th recipe card finished

--- js/recipes.js
+++ js/recipes.js
@@ -1,18 +1,4 @@
-
-//constructor for referance while building objects for referance data might use in later dev for a add recipecard form.
-// the concept would be form taking in text of dish, number of servings, time to make, source(html link),a list of ingrediants(maybe text box to array?), annother array for directions. 
-// function RecipeCard(dish,servingSize,time,source,ingredients,directions){
-//   this.dish = dish;
-//   this.serveingSize = servingSize;
-//   this.time = time;
-//   this.source = source;
-//   this.ingredients = var ingredientList = {
-//     ingrediant:quantity
-//   }
-//   this.directions = [directions]
-//   };
-
-var shepardsPie = {
+var recipeCard1 = {
   dish:"Shepards Pie",
   servingSize:"6",
   time:"1 hour 15 Min",
@@ -20,12 +6,78 @@ var shepardsPie = {
   ingredients:['1lb ground beef or lamb','1 medium yellow onion','1 carrot','2 cloves of garlic minced','1 cup red wine','2 Tbsp of butter','1 Tbsp of olive oil','2 Tbsp of tomato paste','4 potatoes pealed and cubed','4 cups of beef stock','basil','oregano','thyme','salt and pepper','1/8 cup cream','1/4 cup parmesan'],
   directions:['0. Preheat the oven to 400deg.','1. Bring a medium to large pot of salted water to a boil. Add potatoes and cook until tender but still firm, about 15 minutes. Drain and mash. Mix in butter, cream, and parmesan and set aside to cool.',' 2. Heat a skillet with the olive oil, over medium high heat. Sear off your meat "no color no flavor", once it has some color drain it off and shred your onion into your pan.','3. Shred the carrot into the pan and stir, cook until the vegetables are soft and starting to take color(about 4 min).','4. Add in the tomato paste and minced garlic stir a bit while adding the wine. cook off the wine for about 3 to 5 min.','5. slowly stir in the beef stock and bring to a high simmer and let simmer without a lid for 8 min or until thick.','6. Once thickened gently spoon the mashed potatoes on top (use a rubber spatula to spread if needed) then fluff with a fork and sprinkle any extra parmesan on top and bake for 15 min.','7. cool for 15 min and serve.']
 }
-var chickenParm = {
+var recipeCard2 = {
   dish:"Chicken Parmesan",
   servingSize: "4",
-  time:"Prep 25 Min : Cook 20 Min",
+  time:"Prep 25 Min | Cook 20 Min",
   source:"Matt Ravenmoore with credit to Gorden Ramsey for a base",
   ingredients:['4 Skinless, boneless chicken breast','1 cup of parmesan cheese divided in 1/4s','1/2 cup mozzarella sliced','1 cup breadcrumbs','2 large eggs','1 cup of flour','2 Tbsp of olive oil','2 to 4 Tbsp of butter in pats','16oz of Marinara sauce','1/2 cup fresh basil finely chopped','linguini pasta','Salt and pepper'],
   directions:['0.  Preheat Oven to 450 degrees F.','1. set up your trays for the breading, beat the eggs in one, flour in another and mix the breadcrumbs parmesan salt pepper and spicese in the last tray','2. lay out and cut each chicken breast to an escalope (thin butterfly style cut) place between two pieces of parchment then roll with a rolling pin to even out.','3. Apply the bread coating to the escalloped Chicken, Starting with the flour then the egg wash then the bread crumbs.','4. while the chicken rests, Start a pot of salted water for the pasta and heat a frying pan to med-high heat and add the oil.','5. Once hot place your first piece of chicken in and listen for that sizzle, add a bit of butter to the sides of the pan for browning.','6.Flip chicken once browned on one side and brown the second about 2 minutes a side. set on an oven pan to go into the oven ,Repeat steps 5 and 6 for all chicken','7. Take a Tbsp of Marinara and spoon it on to each of the pieces of chicken then layer mozzarella on top and into the oven for 10-15min.','8.Meanwhile Drop your pasta in your now boiling water','9. pull out chicken and set aside for a min to rest then drain pasta and drizzle with a bit of oil and serve.']
-}
+};
+var recipeCard3 = {
+  dish:"Swedish Meatballs",
+  servingSize: "4-6",
+  time:"Prep: 1H 30min | Cook: 35min",
+  source:"All-recipes https://www.allrecipes.com/recipe/231169/chef-johns-swedish-meatballs/?internalSource=hub%20recipe&referringContentType=Search with tweeks by Matt Ravenmoore ",
+  ingredients:['4 Tbsp Butter devided','1/2 Yellow Onion Finely Chopped','1/4 Cup Milk','2 Large Eggs','1/3 Cup Plain Bread Crumbs','1/4 Tsp Ground Nutmeg','1/4 Tsp Ground Allspice','1 Pound Ground Beef','1 Pound Ground Pork','3 tbsp flour','3 1/4 Cups Beef Broth','1/2 Cup Heavy Cream','1/4 Tsp Worcestershire Sauce','1 Tbsp dejon mustard','Salt and Pepper to Taste'],
+  directions:['useing 1/2 of the buter saute the onions in a skilit untill translucent.','Transfer onion mixture to a large bowl. Add milk, eggs, bread crumbs, nutmeg, allspice, and salt and pepper to taste; stir to combine.','Mix beef and pork into the bread crumb mixture. Cover with plastic wrap and refrigerate for 1 hour.','Melt 2 tablespoons butter in a large skillet over medium heat; whisk flour into butter and cook until golden brown, 4 to 5 minutes. Slowly whisk beef broth into butter mixture.','Increase heat to medium-high, whisk cream into mixture and bring to a simmer. Stir mustard and Worcestershire sauce into gravy. Simmer until slightly thickened, 6 to 7 minutes. Season with salt and black pepper. Remove from heat and set aside.',' Preheat oven to 425 degrees F. Line a baking sheet with aluminum foil and lightly coat foil with cooking spray.','Roll 2 to 3 tablespoons meatball mixture into a ball using wet hands; repeat with remaining meatball mixture. Place meatballs on the prepared baking sheet.','Bake in the preheated oven until browned, about 20 minutes. A thermometer inserted into the center should read at least 160 degrees F.','Transfer meatballs to the gravy and cook over medium-low heat until warmed through, about 5 minutes.','Pull from heat, plate and enjoy!']
+};
+var recipeCard4 = {
+  dish:"",
+  servingSize: "",
+  time:"Prep:  | Cook: ",
+  source:" Matt Ravenmoore ",
+  ingredients:[],
+  directions:[]
+};
+var recipeCard5 = {
+  dish:"Spinach and Strawberry Salad",
+  servingSize: "8",
+  time:"Prep: 10 Mins | Cook: 10 Mins",
+  source:"all recipes: https://www.allrecipes.com/recipe/16409/spinach-and-strawberry-salad/?internalSource=hub%20recipe&referringContentType=Search",
+  ingredients:['2 Bunches of Spinach, Rinsed and Shredded to bite-sized pieces','4 Cups Sliced Strawberries','½ Cup Vegetable Oil',' ¼ Cup White Wine vinegar',' ½ Cup White Sugar','¼ Teaspoon Paprika','2 Tablespoons Sesame Seeds (Optional)','1 Tablespoon Poppy Seeds (Optional)'],
+  directions:['In a large bowl, toss together the spinach and strawberries.','In a medium bowl, whisk together the oil, vinegar, sugar, paprika, and seeds.','Pour mixture over the spinach and strawberries, and toss to coat.','Serve and Enjoy!']
+};
+var recipeCard6 = {
+  dish:"",
+  servingSize: "",
+  time:"Prep:  | Cook: ",
+  source:" Matt Ravenmoore ",
+  ingredients:[],
+  directions:[]
+};
+var recipeCard7 = {
+  dish:"",
+  servingSize: "",
+  time:"Prep:  | Cook: ",
+  source:" Matt Ravenmoore ",
+  ingredients:[],
+  directions:[]
+};
+var recipeCard8 = {
+  dish:"",
+  servingSize: "",
+  time:"Prep:  | Cook: ",
+  source:" Matt Ravenmoore ",
+  ingredients:[],
+  directions:[]
+};
+var recipeCard9 = {
+  dish:"",
+  servingSize: "",
+  time:"Prep:  | Cook: ",
+  source:" Matt Ravenmoore ",
+  ingredients:[],
+  directions:[]
+};
+var recipeCard10 = {
+  dish:"",
+  servingSize: "",
+  time:"Prep:  | Cook: ",
+  source:" Matt Ravenmoore ",
+  ingredients:[],
+  directions:[]
+};
+
+
 
