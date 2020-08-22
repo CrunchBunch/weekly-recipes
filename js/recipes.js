61c9bebac85e4a2694c9dfaeef23e6c374be9f13
Matt Ravenmoore <M_ravenmoore@outlook.com>
2020-08-22 10:53:41 -0700

    constructortemplate bulilt and first recipe in

---
+++ js/recipes.js
@@ -0,0 +1,21 @@
+
+//constructor for referance while building objects for referance data might use in later dev for a add recipecard form.
+// the concept would be form taking in text of dish, number of servings, time to make, source(html link),a list of ingrediants(maybe text box to array?), annother array for directions. 
+// function RecipeCard(dish,servingSize,time,source,ingredients,directions){
+//   this.dish = dish;
+//   this.serveingSize = servingSize;
+//   this.time = time;
+//   this.source = source;
+//   this.ingredients = [ingredients]
+//   this.directions = [directions]
+//   };
+
+var shepardsPie = {
+  dish:"Shepards Pie",
+  servingSize:6,
+  time:"1 hour 15 Min",
+  source:"Matt Ravenmoore with credit to Gorden Ramsey for a base",
+  ingredients:['1lb groundbeef or lamb','1 med yellow onion','1 carrot','2 cloves of garlic minced','1 cup red wine','2 Tbsp of butter','1 Tbsp of olive oil','2 Tbsp of tomato paste','4 potatos pealed and cubed','4 cups of beef stock','basil','oreigano','thyme','salt and pepper','1/8 cup cream','1/4 cup parmesan'],
+  directions:['0. Preheat oven to 400deg.','1. Bring a medium to large pot of salted water to a boil. Add potatoes and cook until tender but still firm, about 15 minutes. Drain and mash. Mix in butter, cream, and parmesan and set aside to cool.',' 2. Heat a skillit with the olive oil, over medium high heat. sear off your meat "no color no flavor", once it has some color drain it off and shred your onion in to your pan.','3. Shred the carrot into the pan and stir, cook untill the vegitables are soft and starting to take color(about 4 min).','4. Add in the tomato paste and minced garlic stir a bit while adding the wine. cook off the wine for about 3 to 5 min.','5. slowly stir in the beef stock and bring to a high simmer and let simmer with out a lid for 8 min or untill thick.','6. Once thickened gently spoon the mashed potatos on top (use a ruber spatula to spread if needed) then fluf with a fork and sprinkle any extra parmesan on top and bake for 15 min.','7. cool for 15 min and serve.']
+}
+
