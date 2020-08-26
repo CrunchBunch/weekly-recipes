91b024a8dc8530079e80267083c67eaef07b09f8
Czarl Jalos <czarlfrancis@gmail.com>
2020-08-25 18:22:19 -0700

    added render for vegitarian recipe.

--- js/todays.js
+++ js/todays.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//<===================== Data from Local Storage =============================>
 var recipe;
 var form;
 function loadrecipe(){
@@ -10,37 +10,50 @@ function loadrecipe(){
   form = formInfo;
   
 }
-loadrecipe();
-console.log(recipe);
-console.log(form);
 
+//<===================== User Input for recipe ===============================>
 function showRecipe() {
-  var body = document.getElementById('single-recipe');
+  
   for(var i = 0; i < form.length; i++) {
+  
     for(var j = 0; j < recipe.length; j++) {
       if(form[i].servings === recipe[j].servingSize && form[i].protein === 'Meat' && recipe[j].isMeat === true){
-        var recipeList1 = document.createElement('li');
-        recipeList1.textContent = `${recipe[j].dish}`;
-        body.appendChild(recipeList1);
-        var recipeList2 = document.createElement('li');
-        recipeList2.textContent = `${recipe[j].servingSize}`;
-        body.appendChild(recipeList2);
-        var recipeList3 = document.createElement('li');
-        recipeList3.textContent = `${recipe[j].time}`;
-        body.appendChild(recipeList3);
-        var recipeList4 = document.createElement('li');
-        recipeList4.textContent = `${recipe[j].source}`;
-        body.appendChild(recipeList4);
-        var recipeList5 = document.createElement('li');
-        recipeList5.textContent = `${recipe[j].ingredients}`;
-        body.appendChild(recipeList5);
-        var recipeList6 = document.createElement('li');
-        recipeList6.textContent = `${recipe[j].directions}`;
-        body.appendChild(recipeList6);
-        alert('im here');
+        render(recipe[j]);
       }
-      
+      if(form[i].servings === recipe[j].servingSize && form[i].protein === 'Vegitarian' && recipe[j].isMeat === false){
+        render(recipe[j]);
+      }
     }
   }
 }
+
+
+//<======================== Recipe data Render ===============================>
+
+function render (array) {
+  var body = document.getElementById('single-recipe');
+  var recipeList1 = document.createElement('li');
+  recipeList1.textContent = `${array.dish}`;
+  body.appendChild(recipeList1);
+  var recipeList2 = document.createElement('li');
+  recipeList2.textContent = `${array.servingSize}`;
+  body.appendChild(recipeList2);
+  var recipeList3 = document.createElement('li');
+  recipeList3.textContent = `${array.time}`;
+  body.appendChild(recipeList3);
+  var recipeList4 = document.createElement('li');
+  recipeList4.textContent = `${array.source}`;
+  body.appendChild(recipeList4);
+  var recipeList5 = document.createElement('li');
+  recipeList5.textContent = `${array.ingredients}`;
+  body.appendChild(recipeList5);
+  var recipeList6 = document.createElement('li');
+  recipeList6.textContent = `${array.directions}`;
+  body.appendChild(recipeList6);
+}
+
+
+//<======================= function call ======================================>
+loadrecipe();
 showRecipe();
+console.log(form)
